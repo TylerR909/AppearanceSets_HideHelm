5a6d89500e228bb44e645397c34e943d1bdfef24
Tyler Swavely <tyler.r909@gmail.com>
2017-04-04 08:54:15 -0700

    Some comments to help

--- HelmHide.lua
+++ HelmHide.lua
@@ -1,5 +1,23 @@
 local name, L = ...
 
+-- event: TRANSMOG_COLLECTION_UPDATED
+-- event: TRANSMOG_SEARCH_UPDATED
+-- frame: CollectionsJournal -> WardrobeCollectionFrame
+-- WardrobeCollectionFrame.SetsCollectionFrame.DetailsFrame
+
+--[[
+local chk = CreateFrame("CheckButton","ASHH_Check",WardrobeCollectionFrame.SetsCollectionFrame.DetailsFrame,"UICheckButtonTemplate")
+chk:SetPoint("TOPLEFT",10,-5);
+chk:SetText("Hide Helm");
+chk.tooltip = "Hide helm from apperance sets viewer."
+chk:SetScript("OnClick",
+   function()
+      self:Hide();
+   end
+);
+
+chk:Show() --]]
+
 local function L:HideHelm() 
     WardrobeCollectionFrame.SetsCollectionFrame.Model:UndressSlot(1)
 end
