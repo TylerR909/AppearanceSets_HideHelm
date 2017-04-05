158945c98b5898d49d68f3169ce1815841ee8404
Tyler Swavely <tyler.r909@gmail.com>
2017-04-04 23:17:54 -0700

    OnInit Updated and libraries modified

--- HelmHide.lua
+++ HelmHide.lua
@@ -1,43 +1,39 @@
 local AddOn_Name, ns = ...
 
-ASSH = LibStub("AceAddon-3.0"):NewAddon(AddOn_Name)
+ASHH = LibStub("AceAddon-3.0"):NewAddon(AddOn_Name,"AceEvent-3.0")
 local L = LibStub("AceLocale-3.0"):GetLocale(AddOn_Name,true)
 
 local defaultOptions = {
     name = AddOn_Name,
-    handler = ASSH,
-    type = "group",
+    handler = ASHH,
+    type = 'group',
     args = {
         enable = {
             name = "Enable",
             desc = "Enables/Disables the addon",
-            type = "toggle",
-            set = function(info, val) MyAddon.enable end,
-            get = function(info) return MyAddon.enabled end
+            type = 'toggle',
+            hidden = true,
+            set = function(info, val) end,
+            get = function(info) return end
+        },
+        helmDefault = {
+            name = "Hide Helm Default",
+            desc = "Hide helm by default?",
+            type = 'toggle',
+            order = 0,
+            set = function(info, val) end, -- do something?
+            get = function(info) end -- return something here
+        },
+        expandVariants = {
+            name = "Expand Variants",
+            desc = "Expand variants from dropdown to buttons?",
+            type = 'toggle',
+            set = function(info, val) end, -- definitely do something 
+            get = function(info) end
         }
-        moreOptions = {
-            name = "More options",
-            type = "group",
-            args = {
-                helmDefault = {
-                    name = "Hide Helm Default",
-                    desc = "Hide helm by default?",
-                    type = "toggle",
-                    set = function(info, val) end; -- do something?
-                    get = function(info) end -- return something here
-                }
-                expandVariants = {
-                    name = "Expand Variants",
-                    desc = "Expand variants from dropdown to buttons?"
-                    type = "toggle",
-                    set = function(info, val) end; -- definitely do something 
-                    get = function(info) end
-                }
-                -- TODO: Differentiate between global setting and per-character setting. Some toons may actually like helms.
-                -- TODO: Add shoulders because people hate those too I guess -_-
-                -- TODO: Way in the future, just add this as a setting to the dressing room to remove any helms tried on "anywhere"
-            }
-        }
+            -- TODO: Differentiate between global setting and per-character setting. Some toons may actually like helms.
+            -- TODO: Add shoulders because people hate those too I guess -_-
+            -- TODO: Way in the future, just add this as a setting to the dressing room to remove any helms tried on "anywhere"
     }
 }
 
@@ -60,22 +56,33 @@ chk:SetScript("OnClick",
 
 chk:Show() --]]
 
-function ASSH:HideHelm() 
+function ASHH:HideHelm() 
     WardrobeCollectionFrame.SetsCollectionFrame.Model:UndressSlot(1)
 end
 
-function ASSH:OnInitialize()
-    self.db = LibStub("AceDB-3.0"):New("ASHHDB",defaultOptions)
-    -- Build CheckButton
+function ASHH:HookButtons()
+    -- Build button, etc
+end
+
+function ASHH:OnInitialize()
+    -- self.db = LibStub("AceDB-3.0"):New("ASHHDB")
+    -- LibStub("AceConfig-3.0"):RegisterOptionsTable(AddOn_Name,defaultOptions) -- options is wrong?
+    -- self.optionsFrame = LibStub("AceConfigDialog-3.0"):AddToBlizOptions(AddOn_Name,AddOn_Name)
+    self:RegisterEvent("ADDON_LOADED", function (self, addon, ...)
+        if addon == "Blizzard_Collections" then
+            ASHH:HookButtons()
+            ASHH:UnregisterEvent("ADDON_LOADED")
+        end
+    end)
     -- Start looking for variants?
 end
 
-function ASSH:OnEnable()
+function ASHH:OnEnable()
     -- Attach checkbox
     -- Start looking to convert variants
 end
 
-function ASSH:OnDisable()
+function ASHH:OnDisable()
     -- Disable checkbox
     -- Stop looking for variants
 end
