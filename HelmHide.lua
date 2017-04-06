c374432cf2b019f8fba69d756b32d70bbe9d951a
Tyler Swavely <tyler.r909@gmail.com>
2017-04-05 23:46:57 -0700

    Many small tweaks and upgrade prep - Fixed default options - Prepared options table for option toggling - Added groundwork for hide shoulders/back - changed default values to load from memory instead of hard-coded - Hide helm when the model loads if checked

--- HelmHide.lua
+++ HelmHide.lua
@@ -1,10 +1,24 @@
 local AddOn_Name, ns = ...
+local elap_buffer = 0.05
 
 ASHH = LibStub("AceAddon-3.0"):NewAddon(AddOn_Name,"AceEvent-3.0")
 local AceGUI = LibStub("AceGUI-3.0")
 local L = LibStub("AceLocale-3.0"):GetLocale(AddOn_Name,true)
 
 local defaultOptions = {
+    global = {
+        hideHelm = true,
+        hideShoulders = false,
+        hideBack = false
+    },
+    char = {
+        hideHelm = true,
+        hideShoulders = false,
+        hideBack = false
+    }
+}
+
+local optionsTable = {
     name = AddOn_Name,
     handler = ASHH,
     type = 'group',
@@ -14,16 +28,30 @@ local defaultOptions = {
             desc = "Enables/Disables the addon",
             type = 'toggle',
             hidden = true,
-            set = function(info, val) end,
-            get = function(info) return end
+            set = function(self, val) end,
+            get = function(self) return end
         },
         helmDefault = {
             name = "Hide Helm Default",
             desc = "Hide helm by default?",
             type = 'toggle',
             order = 0,
-            set = function(info, val) end, -- do something?
-            get = function(info) end -- return something here
+            set = function(self, val) ASHH.db.char.hideHelm = self:GetChecked() end, -- do something?
+            get = function(self) return ASHH.db.char.hideHelm end -- return something here
+        },
+        shoulderDefault = {
+            name = "Shoulder Hide Default",
+            desc = "Hide shoulder by default?",
+            type = 'toggle',
+            set = function(self, val) ASHH.db.char.hideShoulder = self:GetChecked() end,
+            get = function(self) return ASHH.db.char.hideShoulder end
+        },
+        backDefault = {
+            name = "Back Hide Default",
+            desc = "Hide back by default?",
+            type = 'toggle',
+            set = function(self, val) ASHH.db.char.hideBack = self:GetChecked() end,
+            get = function(self) return ASHH.db.char.hideBack end
         },
         expandVariants = {
             name = "Expand Variants",
@@ -61,13 +89,21 @@ function ASHH:HideHelm()
     WardrobeCollectionFrame.SetsCollectionFrame.Model:UndressSlot(1)
 end
 
+function ASHH:HideShoulders()
+    WardrobeCollectionFrame.SetsCollectionFrame.Model:UndressSlot(3)
+end
+
+function ASHH:HideBack()
+    WardrobeCollectionFrame.SetsCollectionFrame.Model:UndressSlot(15)
+end
+
 function ASHH:CreateButtons()
     -- Build button, etc
     ASHH.buttons = ASHH.buttons or {}
 
     local hh = CreateFrame("CheckButton",nil,WardrobeCollectionFrame.SetsCollectionFrame.DetailsFrame,"UICheckButtonTemplate")
     hh:SetPoint("TOPLEFT",5,0)
-    hh:SetChecked(false) -- TODO: Default
+    hh:SetChecked(self.db.char.hideHelm) -- TODO: Default
     hh:SetText("Hide Helm")
     hh.tooltip = "Hides the helm when you load a new set"
     -- TODO: Click works, but Arrow Keys to select fails to trigger
@@ -81,14 +117,22 @@ function ASHH:CreateButtons()
         end
     end)
 
-    ASHH.buttons.helmhide = hh
+    hh:SetScript("OnShow", function(self)
+        if self:GetChecked() then
+            ASHH:HideHelm()
+        end
+    end)
+
+
+    ASHH.buttons.hideHelm = hh
 end
 
 function ASHH:HookScripts()
+    --Hook to Set Buttons
     local btn_h = "WardrobeCollectionFrameScrollFrameButton"
     local count = 1
     local keepGoing = true
-    local hh = ASHH.buttons.helmhide
+    local hh = ASHH.buttons.hideHelm
 
     local btn = _G["WardrobeCollectionFrameScrollFrameButton"..count]
 
@@ -105,11 +149,27 @@ function ASHH:HookScripts()
         count = count + 1
         btn = _G["WardrobeCollectionFrameScrollFrameButton"..count]
         if not btn then keepGoing = false end
-   end
+    end
+
+   -- Hook to Model Frame
+    WardrobeCollectionFrame.SetsCollectionFrame.Model:HookScript("OnShow",function(self)
+        local elap = 0
+
+        ASHH.buttons.hideHelm:SetScript("OnUpdate",function (self, elapsed)
+            elap = elap + elapsed
+            if elap > elap_buffer then
+                if ASHH.buttons.hideHelm:GetChecked() then
+                    ASHH:HideHelm()
+                    self:SetScript("OnUpdate",nil)
+                end
+            end
+        end)
+        --register ASHH.buttons.hideHelm for onUpdate
+    end)
 end
 
 function ASHH:OnInitialize()
-    -- self.db = LibStub("AceDB-3.0"):New("ASHHDB")
+    self.db = LibStub("AceDB-3.0"):New("ASHHDB",defaultOptions,true)
     -- LibStub("AceConfig-3.0"):RegisterOptionsTable(AddOn_Name,defaultOptions) -- options is wrong?
     -- self.optionsFrame = LibStub("AceConfigDialog-3.0"):AddToBlizOptions(AddOn_Name,AddOn_Name)
     self:RegisterEvent("ADDON_LOADED", function (self, addon, ...)
