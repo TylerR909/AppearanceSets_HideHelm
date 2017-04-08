9fb094ac83718a2b32d93d38d1fc5118e7ff8f78
Tyler Swavely <tyler.r909@gmail.com>
2017-04-08 14:43:45 -0700

    Added Russian

---
+++ Locale/ruRU.lua
@@ -0,0 +1,16 @@
+-- Contributors: BLizzatron@Curse
+
+local L = LibStub("AceLocale-3.0"):NewLocale("AppearanceSets_HideHelm", "ruRU", false)
+
+if not L then return end
+
+L["Hide Helm"] = "Скрыть шлем"
+L["Hide Shoulders"] = "Скрыть плечи"
+L["Hide Back"] = "Скрыть плащ"
+L["Hide Belt"] = "Скрыть пояс"
+L["Global Defaults"] = "Глобальные настройки по умолчанию"
+L["Character Defaults"] = "Для персонажа по умолчанию"
+L["Use Global"] = "Использовать глобальные"
+L["Expand Variants"] = "Расширенные варианты"
+L["Coming soon!"] = "Скоро!"
+L["ReloadMsg"] = "Изменения вступят в силу на следующей перезагрузки"
